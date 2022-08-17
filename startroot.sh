d0f2d7a83cc78cbf392a9515da45601ae37204e1
JohnBroksGithub <107406542+JohnBroksGithub@users.noreply.github.com>
2022-08-17 12:51:52 +0700

    Create startroot.sh

---
+++ startroot.sh
@@ -0,0 +1,20 @@
+# This Projects Made By Idkwannawhatname#1724 And XStriver
+# This Script Basicly Will Install Proot Root Which needed to install Desktop
+# This Script Auto delete useless File
+echo Started Script!
+echo Dont Forget To Sub And Like
+wget https://cdn.discordapp.com/attachments/853535040250970113/878590395611775016/yt.zip
+unzip yt.zip
+unzip root.zip
+tar -xvf root.tar.xz
+echo Deleting File To save Some disk
+echo its takes 1min!
+rm music.mp3
+rm root.sh
+rm root.tar.xz
+rm root.zip
+rm video.mp4
+rm yt.zip
+echo done clean some useless file
+echo starting proot root
+./dist/proot -S . /bin/bash
