d76b0268e05ffa3e1a1caf03baa16a5b777a454c
JohnBroksGithub <107406542+JohnBroksGithub@users.noreply.github.com>
2022-08-17 12:58:04 +0700

    Create installdesktop.sh

---
+++ installdesktop.sh
@@ -0,0 +1,6 @@
+# This Script  Made By Idkwannawhatname#1724
+echo dont forget sub and like
+apt update
+apt install wget x11vnc xvfb fluxbox firefox-esr -y
+echo done!
+echo run script in from video tutorial!
